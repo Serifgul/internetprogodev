93ba1fd1ac110a7ad092382749fd93ddc923bdba
Serifgul <serif.gul.449@gmail.com>
2025-05-13 15:28:16 +0300

    .
    .

--- HepsiseriftClient/src/components/layout/Header.jsx
+++ HepsiseriftClient/src/components/layout/Header.jsx
@@ -28,8 +28,12 @@ const Header = () => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const data = await getCategories();
-        setCategories(data);
+          const data = await getCategories();
+          console.log(data.values);
+          setCategories(data.values);
+
+
+          console.log(categories.values);
       } catch (error) {
         console.error('Error fetching categories:', error);
       }
@@ -105,15 +109,15 @@ const Header = () => {
               {isCategoryMenuOpen && (
                 <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
                   <div className="py-1">
-                    {categories.map((category) => (
-                      <Link
-                        key={category.id}
-                        to={`/products/category/${category.id}`}
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        onClick={() => setIsCategoryMenuOpen(false)}
-                      >
-                        {category.name}
-                      </Link>
+                   {categories.map((category) => (
+                                          <Link
+                                              key={category.id}
+                                              to={`/products/category/${category.id}`}
+                                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                              onClick={() => setIsCategoryMenuOpen(false)}
+                                          >
+                                              {category.name}
+                                          </Link>
                     ))}
                   </div>
                 </div>
